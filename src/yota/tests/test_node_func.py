7efb1e9bc9022cd9fa3f0ab41ad590c6758b9875
Isaac Cook <isaac@simpload.com>
2013-06-11 11:26:25 -0500

    Fix for #23 and functional test to confirm

--- src/yota/tests/test_node_func.py
+++ src/yota/tests/test_node_func.py
@@ -35,5 +35,21 @@ class TestBuiltinNodes(unittest.TestCase):
         assert(len(bs.findAll('option', {'value': '2'})) == 1)
         assert(len(bs.findAll('option')) == 3)
 
+    def test_textarea(self):
+        """ Tests our textarea node for containing fields and content"""
+        class TForm(yota.Form):
+            t = TextareaNode(rows='15', columns='20')
+
+        test = TForm(auto_start_close=False).render()
+        bs = BeautifulSoup(test)
+        assert(len(bs.findAll('textarea', {'rows': '15', 'cols': '20'})) == 1)
+
+        class TForm(yota.Form):
+            t = TextareaNode(rows='15', columns='20')
+            _t_long = Check(MinLengthValidator(5), 't')
+        test = TForm(auto_start_close=False).validate_render({'t': 'test'})
+        bs = BeautifulSoup(test)
+        assert('test' in bs.findAll('textarea')[0].contents[0].strip())
+
 if __name__ == '__main__':
     unittest.main()
